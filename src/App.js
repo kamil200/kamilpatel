3545d4ff9e4898ee8252a3628445c3f1b363fd45
patel kamil <kamilp786@gmail.com>
2023-06-21 03:01:25 +0530

    0.1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div
+        class="tenor-gif-embed"
+        data-postid="21528295"
+        data-share-method="host"
+        data-aspect-ratio="1.66667"
+        data-width="100%"
+      >
+        <a href="https://tenor.com/view/panchayat-karne-ki-aadat-gayi-nahi-teri-jethalal-chapaklal-gada-tarak-mehta-ka-ooltah-chashmah-gif-21528295">
+          Panchayat Karne Ki Aadat Gayi Nahi Teri Jethalal Chapaklal Gada GIF
+        </a>
+        from{" "}
+        <a href="https://tenor.com/search/panchayat+karne+ki+aadat+gayi+nahi+teri-gifs">
+          Panchayat Karne Ki Aadat Gayi Nahi Teri GIFs
+        </a>
+      </div>{" "}
+      <script
+        type="text/javascript"
+        async
+        src="https://tenor.com/embed.js"
+      ></script>
+    </div>
+  );
+}
+
+export default App;
